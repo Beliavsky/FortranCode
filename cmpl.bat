e0c9896ccda9c1dbebc78b6c5515f848184e6f85
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2024-08-31 16:46:14 -0400

    Update cmpl.bat

--- cmpl.bat
+++ cmpl.bat
@@ -1,2 +1,2 @@
-gfortran string_funcs.f90 xstring_funcs.f90
-gfortran kind.f90 binomial.f90 xbinomial.f90
+gfortran -o xstring_funcs.exe string_funcs.f90 xstring_funcs.f90
+gfortran -o xbinomial.exe kind.f90 binomial.f90 xbinomial.f90
