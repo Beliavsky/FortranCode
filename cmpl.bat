b8e50c34b34c1a086a4ed213a1fa8d7c5b3a2d6e
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2024-10-06 18:16:22 -0400

    added compilation command for xxrandom.f90

--- cmpl.bat
+++ cmpl.bat
@@ -1,3 +1,4 @@
 gfortran -o xstring_funcs.exe string_funcs.f90 xstring_funcs.f90
 gfortran -o xbinomial.exe kind.f90 binomial.f90 xbinomial.f90
 gfortran -o xsim_negative_binomial.exe kind.f90 binomial.f90 xsim_negative_binomial.f90
+gfortran -o xxrandom.exe kind.f90 random.f90 xxrandom.f90
